57ea39ac2ab09be68788968cc98483df5fcde30f
8778saquib <saquib.nawaz009@gmail.com>
2022-05-22 22:51:31 +0530

    desk of card

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeckOfCards deckOfCards = new DeckOfCards();
+            deckOfCards.Shuffle();
+            deckOfCards.Display();
+        }
+    }
+}
